5035353937306a0fded8b17cf14a343064e8a714
supriya-premkumar <supriya@fastmail.com>
2018-03-11 14:54:23 -0700

    Updated urls.py by adding social-auth-app-django url

--- fileupload/urls.py
+++ fileupload/urls.py
@@ -1,5 +1,8 @@
 # encoding: utf-8
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth import views
+from .views import home # might need to update
 from fileupload.views import (
         BasicVersionCreateView, BasicPlusVersionCreateView,
         jQueryVersionCreateView, AngularVersionCreateView,
@@ -7,6 +10,13 @@ from fileupload.views import (
         )
 
 urlpatterns = [
+    # AUTH
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
+    url(r'^$', home, name='home'),
+    
     url(r'^basic/$', BasicVersionCreateView.as_view(), name='upload-basic'),
     url(r'^basic/plus/$', BasicPlusVersionCreateView.as_view(), name='upload-basic-plus'),
     url(r'^new/$', PictureCreateView.as_view(), name='upload-new'),
