55d73f76ea6ad61572f79982a6f82adb26fd6a05
supriya-premkumar <supriya@fastmail.com>
2018-03-11 14:40:25 -0700

    Added social-auth-app-django to requirements.txt and social_django and updated context_processors in settings.py

--- django-jquery-file-upload/settings.py
+++ django-jquery-file-upload/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -43,7 +44,7 @@ ROOT_URLCONF = 'django-jquery-file-upload.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        #'DIRS': ['django-jquery-file-upload/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -51,6 +52,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
